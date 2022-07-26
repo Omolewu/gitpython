1a2e5e6c0d82a4f6d5a6b2bc59b5316a128470f8
Omolewu Sunday <omolewu12@gmail.com>
2022-07-26 12:18:23 +0100

    Stop at Logical operators

---
+++ script.js
@@ -0,0 +1,69 @@
+// Operators 
+
+/* Arithmentic Operators
++
+/
+*
+-
+**
+% 
+*/
+
+// let a = 2;
+// let b = 10;
+// console.log(a + b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a - b)
+// console.log(a ** b)
+// console.log(a % b)
+
+/* Assignment Operators
+
+=
++=
+*=
+%=
+/=
+**=
+*/
+// let k = 23;
+// k **= 10;
+// console.log(k);
+// let firstName = "Sunday";
+// firstName += "Opeyemi";
+// console.log(firstName)
+
+/* Comparison operators
+
+==
+<
+>
+>=
+<=
+!=
+===
+!==
+? ternary operator
+*/
+// let a = "23";
+// let b = 23;
+// console.log(a !== b);
+// var score = 78;
+// let grade = score > 70 ? "A" : "Fail";
+let firstName = "Mariam";
+let greeting = firstName == "Mariam" ? "Welcome to OIC Hub " + firstName : "You are not part of us " + firstName;
+
+console.log(greeting);
+
+/* Logical Operators
+
+&&
+||
+!
+
+*/
+
+console.log(!(firstName == "Mariam" && firstName == "Aminat"))
+
+
