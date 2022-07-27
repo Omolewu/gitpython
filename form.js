97947784e0d3d48e17a7b9ee02a56138ab11b97a
Omolewu Sunday <omolewu12@gmail.com>
2022-07-27 12:01:43 +0100

    adding function

---
+++ form.js
@@ -0,0 +1,4 @@
+document.getElementById('userName').value = "Adeojo Mariam";
+// console.log(userName);
+
+console.log(document.getElementById('reg').innerHTML)
---
+++ function.js
@@ -0,0 +1,49 @@
+// function
+
+/*  Function syntaxs
+
+function functionname(){
+    Codes
+}
+*/
+
+// function greeting() {
+//     console.log('Welcome to our Hub');
+// }
+// greeting()
+
+// function welcome() {
+//     document.getElementById('welcome').innerHTML = "Welcome to Javascript class";
+// }
+
+// function getOne() {
+//     document.getElementById('dice').src = "images/d1.png";
+// }
+// function getTwo() {
+//     document.getElementById('dice').src = "images/d2.png";
+// }
+// function getThree() {
+//     document.getElementById('dice').src = "images/d3.png";
+// }
+// function getFour() {
+//     document.getElementById('dice').src = "images/d4.jpeg";
+// }
+// function getFive() {
+//     document.getElementById('dice').src = "images/d5.png";
+// }
+// function getSix() {
+//     document.getElementById('dice').src = "images/d6.jpg";
+// }
+
+function sum(a, b, c) {
+
+    let d = a + b;
+    alert(d);
+
+}
+
+let add = sum(34, 56, 78);
+
+function greet(name) {
+    document.getElementById('welcome').innerHTML = "<p>Welcome to Js class" + name + "</p>";
+}
